63011e8ef9840a6ab32a3d41ec31324328eb5428
sgoc0037 <wazqwe.joker@mail.ru>
2022-05-28 19:00:29 +0400

    add logout

--- src/Components/headerMenu.tsx
+++ src/Components/headerMenu.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import { Menu } from 'antd';
+import { Button, Menu } from 'antd';
 import type { MenuProps } from 'antd';
 import { Link } from 'react-router-dom';
 import { path } from '../Types/RoutesType';
 import { getItem } from '../util/util';
 import { Header } from 'antd/lib/layout/layout';
+import { useAppSelector } from '../app/hooks';
+import { useEffect } from 'react';
 
 type MenuItem = Required<MenuProps>['items'][number];
 
-const items: MenuItem[] = [
-    getItem(<Link to={path.ADMIN}>Admin</Link>, '1'),
-    getItem(<Link to={path.LOGIN}>Login</Link>, '2'),
-    getItem(<Link to={path.PRODUCTS}>Products</Link>, '3'),
-];
-
 export const HeaderMenu: React.FC = () => {
+
+    const isAuth = useAppSelector(state => state.isAuth.isAuth)
+
+    const items: MenuItem[] = [
+        getItem(<Link to={path.ADMIN}>Admin</Link>, '1'),
+        getItem(<Link to={path.LOGIN}>Login</Link>, '2'),
+        getItem(<Link to={path.PRODUCTS}>Products</Link>, '3'),
+    ];
+
+    isAuth && items.push(getItem(<Button type='primary'>Выйти</Button>, 'logout'))
 
     return (
         <Header>
